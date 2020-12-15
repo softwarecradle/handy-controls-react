91c5410fbb2b223a0a513406bcdef2b184ab4df1
Debadeep <debadeep.sen@bandhan.org>
2020-12-15 18:22:22 +0530

    testing progress

--- src/index.js
+++ src/index.js
@@ -4,3 +4,15 @@ import styles from './styles.module.css'
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export const Card = ({ imgSource, text }) => {
+  return (
+    <div>
+      <div style={{ background: `url(${imgSource})`, backgroundPosition: "center", backgroundSize: "cover", width: "100%", margin: 10 }}>
+      </div>
+      <div>
+        {text}
+      </div>
+    </div>
+  )
+}
